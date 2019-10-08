e2b38778de49b339bc99ecb4be7eb10c41e817b1
Haseeb6776 <41138641+Haseeb6776@users.noreply.github.com>
2019-10-08 15:46:58 +0500

    jkjjk

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,6 +10,53 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+
+    $('.slick1').slick({
+      infinite:true,
+      dots: false,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      nextArrow: '<i class="fa fa-chevron-left" ></i>',
+      prevArrow: '<i class="fa fa-chevron-right" ></i>', 
+    });
+
+    $('.slick2').slick({
+      infinite:true,
+      dots: false,
+      speed: 300,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      nextArrow: '<i class="fa fa-chevron-left" ></i>',
+      prevArrow: '<i class="fa fa-chevron-right" ></i>',
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ] 
+    });
+
   }
 
 }
