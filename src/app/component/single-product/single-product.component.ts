970dbc28395b23830043a631a3e510a549fb3028
Haseeb6776 <41138641+Haseeb6776@users.noreply.github.com>
2019-11-07 16:58:47 +0500

    home page

--- src/app/component/single-product/single-product.component.ts
+++ src/app/component/single-product/single-product.component.ts
@@ -9,7 +9,55 @@ export class SingleProductComponent implements OnInit {
 
   constructor() { }
 
+  
+  slideConfig = {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    prevArrow: '<button class="slick-main-btn-left"><i class="fa fa fa-chevron-left"></i></button>',
+    nextArrow: '<button class="slick-main-btn-right"><i class="fa fa-chevron-right"></i></button>',
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1154,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 942,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 730,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 512,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+
+    ]
+  };
+
+  
+
   ngOnInit() {
   }
 
+
+
+  
 }
